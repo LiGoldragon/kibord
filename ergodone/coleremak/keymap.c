458a3a9eeea1b3f77cca7159efa7b65e442adb48
li <@li:maisiliym.uniks>
2021-04-24 04:43:47 +0000

    (mod ralt)

--- ergodone/coleremak/keymap.c
+++ ergodone/coleremak/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        KC_LBRC,   KC_BSLS,   KC_Y,          KC_U,           KC_I,     KC_O,          KC_P,                    
                                   KC_QUOTE,  KC_H,          KC_J,           KC_K,     RCTL_T(KC_L),  KC_SLSH,         
                        KC_RBRC,   KC_ENT,    RGUI_T(KC_N),  KC_M,           KC_COMM,  KC_DOT,        MT(MOD_RSFT,KC_SCLN),
-                                             KC_RALT,       LT(FN,KC_ESC),  KC_DOWN,  KC_UP,         KC_EQL,
+                                             RALT_T(RALT(KC_SPC)),       LT(FN,KC_ESC),  KC_DOWN,  KC_UP,         KC_EQL,
              KC_INS,    KC_DEL,
              KC_PGUP,
              KC_PGDN,   KC_ENT,  KC_TAB
@@ -106,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                        KC_LBRC,   KC_BSLS,   KC_J,          KC_L,           KC_U,     KC_Y,          KC_SCLN,
                                   KC_QUOTE,  KC_H,          KC_N,           KC_E,     RCTL_T(KC_I),  KC_SLSH,
                        KC_RBRC,   KC_ENT,    RGUI_T(KC_K),  KC_M,           KC_COMM,  KC_DOT,        MT(MOD_RSFT,KC_O),
-                                             KC_RALT,       LT(FN,KC_ESC),  KC_DOWN,  KC_UP,         KC_EQL,
+                                             RALT_T(RALT(KC_SPC)),       LT(FN,KC_ESC),  KC_DOWN,  KC_UP,         KC_EQL,
              KC_INS,    KC_DEL,
              KC_PGUP,
              KC_PGDN,   KC_ENT,  KC_TAB
